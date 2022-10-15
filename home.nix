2288e441295a4a5d30fe6adf41b064c3adca2bf6
Archit Gupta <accelbread@gmail.com>
2022-10-14 19:33:27 -0700

    flake: Move dconf to separate file

--- home.nix
+++ home.nix
@@ -2,6 +2,7 @@ user:
 { config, pkgs, lib, ... }:
 with builtins;
 with lib; {
+  imports = [ (import ./dconf.nix) ];
   home = {
     username = user.name;
     homeDirectory = user.home;
@@ -105,76 +106,5 @@ with lib; {
     name = "Manage Printing";
     noDisplay = true;
     exec = null;
-  };
-
-  dconf.settings = with hm.gvariant; {
-    "org/gnome/desktop/background" = {
-      color-shading-type = "solid";
-      picture-options = "zoom";
-      picture-uri =
-        "file:///run/current-system/sw/share/backgrounds/gnome/adwaita-l.jpg";
-      picture-uri-dark =
-        "file:///run/current-system/sw/share/backgrounds/gnome/adwaita-d.jpg";
-      primary-color = "#3465a4";
-      secondary-color = "#000000";
-    };
-    "org/gnome/desktop/input-sources" = {
-      xkb-options = [ "terminate:ctrl_alt_bksp" "compose:caps" ];
-    };
-    "org/gnome/desktop/interface" = {
-      clock-format = "12h";
-      clock-show-weekday = true;
-      color-scheme = "prefer-dark";
-      gtk-theme = "Adwaita-dark";
-      font-antialiasing = "rgba";
-      font-hinting = "slight";
-      locate-pointer = true;
-    };
-    "org/gnome/desktop/media-handling" = { autorun-never = true; };
-    "org/gnome/desktop/peripherals/touchpad" = { speed = 0.4; };
-    "org/gnome/desktop/privacy" = {
-      old-files-age = mkUint32 30;
-      recent-files-max-age = -1;
-      remember-recent-files = false;
-      remove-old-temp-files = true;
-      remove-old-trash-files = true;
-    };
-    "org/gnome/desktop/screensaver" = {
-      color-shading-type = "solid";
-      lock-delay = mkUint32 30;
-      picture-options = "zoom";
-      picture-uri =
-        "file:///run/current-system/sw/share/backgrounds/gnome/adwaita-l.jpg";
-      primary-color = "#3465a4";
-      secondary-color = "#000000";
-    };
-    "org/gnome/desktop/session" = { idle-delay = mkUint32 180; };
-    "org/gnome/desktop/wm/preferences" = { resize-with-right-button = true; };
-    "org/gnome/settings-daemon/plugins/color" = {
-      night-light-enabled = true;
-      night-light-temperature = mkUint32 4226;
-    };
-    "org/gnome/nautilus/preferences" = { show-delete-permanently = true; };
-    "org/gnome/settings-daemon/plugins/power" = {
-      power-button-action = "interactive";
-      sleep-inactive-ac-timeout = 900;
-      sleep-inactive-ac-type = "hibernate";
-      sleep-inactive-battery-timeout = 900;
-      sleep-inactive-battery-type = "hibernate";
-    };
-    "org/gnome/shell" = {
-      disable-user-extensions = false;
-      enabled-extensions = with pkgs.gnomeExtensions; [
-        native-window-placement.extensionUuid
-        system-action-hibernate.extensionUuid
-      ];
-      favorite-apps = [
-        "emacsclient.desktop"
-        "librewolf.desktop"
-        "org.gnome.Nautilus.desktop"
-      ];
-    };
-    "org/gnome/system/location" = { enabled = true; };
-    "org/gtk/settings/file-chooser" = { clock-format = "12h"; };
   };
 }
