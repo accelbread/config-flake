9a4c9e8da217883127b5c23ab14c8ca4b376aab5
Archit Gupta <archit@accelbread.com>
2022-11-06 21:34:26 -0800

    emacs: Use elpaBuild for flymake-vale

--- packages/elisp-packages/flymake-vale.nix
+++ packages/elisp-packages/flymake-vale.nix
@@ -1,16 +1,14 @@
-{ trivialBuild
+{ elpaBuild
 , fetchFromGitHub
 }:
-trivialBuild {
+elpaBuild rec {
   pname = "flymake-vale";
-  src = fetchFromGitHub {
+  version = "0.0.1";
+
+  src = (fetchFromGitHub {
     owner = "tpeacock19";
-    repo = "flymake-vale";
+    repo = pname;
     rev = "27f070b9da6daa1b825d2f06cd3a8ceb4d0c2af8";
     sha256 = "sha256-/qs3BXm4un5FSWOEZ1NoLwGmzIypY/MmYnDpmEnEL+s=";
-  };
-  postBuild = ''
-    emacs -L . --batch -l package --eval \
-      '(package-generate-autoloads "flymake-vale" ".")'
-  '';
+  }) + "/${pname}.el";
 }
