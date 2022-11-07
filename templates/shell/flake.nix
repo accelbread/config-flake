dc58e6235e4af8f4ca72d66e81a1eea7169d56ed
Archit Gupta <archit@accelbread.com>
2022-11-06 17:54:23 -0800

    flake: Fix shell template

--- templates/shell/flake.nix
+++ templates/shell/flake.nix
@@ -1,6 +1,8 @@
 {
-  nixpkgs.url = "nixpkgs/nixos-22.05";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-22.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
