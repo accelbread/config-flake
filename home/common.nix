0d62b817c369d813bb591b86f8cf36563749bf09
Archit Gupta <archit@accelbread.com>
2022-11-10 19:37:03 -0800

    home: Add linux/posix man pages

--- home/common.nix
+++ home/common.nix
@@ -6,7 +6,17 @@
       TPM2_PKCS11_STORE = "$HOME/.local/share/tpm2_pkcs11";
       TSS2_LOG = "fapi+NONE";
     };
-    packages = with pkgs; [ git ripgrep fd tree jq zeal podman ];
+    packages = with pkgs; [
+      man-pages
+      man-pages-posix
+      git
+      ripgrep
+      fd
+      tree
+      jq
+      zeal
+      podman
+    ];
   };
 
   fonts.fontconfig.enable = true;
