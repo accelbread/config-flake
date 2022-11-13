00f7cef4342d04acc4542eb767446774ccd85d1b
Archit Gupta <accelbread@gmail.com>
2022-11-12 16:03:29 -0800

    template: Make shell template use nix registry

--- templates/shell/flake.nix
+++ templates/shell/flake.nix
@@ -1,8 +1,4 @@
 {
-  inputs = {
-    nixpkgs.url = "nixpkgs/nixos-22.05";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
