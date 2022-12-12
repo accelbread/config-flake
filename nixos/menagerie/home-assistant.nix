1533010a40ad3c93b2e65b27a3095b5d8448cae8
Archit Gupta <archit@accelbread.com>
2022-12-11 20:31:18 -0800

    menagerie: Add backup DNS due to dependency between dnscrypt and NTP

--- nixos/menagerie/home-assistant.nix
+++ nixos/menagerie/home-assistant.nix
@@ -1,7 +1,22 @@
-{ pkgs, flakes, hostname, ... }: {
+{ config, pkgs, flakes, hostname, ... }: {
   imports = [ flakes.nixos-hardware.nixosModules.raspberry-pi-4 ];
 
   boot.loader.generic-extlinux-compatible.enable = false;
+
+  # DNSCrypt-proxy needs NTP which needs DNS; use public resolver for boot
+  networking.nameservers = [ "1.1.1.1" ];
+
+  systemd.services.enable-local-resolver =
+    assert config.networking.resolvconf.enable; {
+      description = "Set DNS resolver to localhost";
+      after = [ "time-sync.target" ];
+      wantedBy = [ "multi-user.target" ];
+      script = ''
+        ${pkgs.openresolv}/sbin/resolvconf -m 1 -a static <<EOF
+        nameserver 127.0.0.1
+        EOF
+      '';
+    };
 
   services.home-assistant = {
     enable = true;
