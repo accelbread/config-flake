096732fa4ca157c188b32ee9dbc6ac2645f1e4e2
Archit Gupta <archit@accelbread.com>
2022-12-28 00:17:30 -0800

    templates: Add new template for C applications

---
+++ templates/c-bin/flake.nix
@@ -0,0 +1,46 @@
+# Template C application
+# Copyright (C) 2023 Archit Gupta
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+{
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      eachSystem = with nixpkgs.lib; f: foldAttrs mergeAttrs { }
+        (map (s: mapAttrs (_: v: { ${s} = v; }) (f s)) systems);
+    in
+    eachSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "hello_world";
+          src = ./.;
+          installPhase = "make DESTDIR=$out install";
+          meta = with pkgs.lib; {
+            description = "Template C application project.";
+            license = licenses.agpl3Plus;
+            platforms = platforms.linux;
+          };
+        };
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [ clang-tools ];
+        };
+        formatter = pkgs.nixpkgs-fmt;
+      });
+}
--- templates/default.nix
+++ templates/default.nix
@@ -2,10 +2,14 @@ rec {
   default = shell;
   shell = {
     path = ./shell;
-    description = "Template for `nix develop` and direnv.";
+    description = "Template for Nix devShells.";
   };
   elisp-package = {
     path = ./elisp-package;
-    description = "Template for new emacs packages.";
+    description = "Template for new Emacs packages.";
+  };
+  c-bin = {
+    path = ./c-bin;
+    description = "Template for C applications.";
   };
 }
