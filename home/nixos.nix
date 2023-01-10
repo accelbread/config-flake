8f0149335280f2c3f787db1050aad1db9e3c8eb0
Archit Gupta <archit@accelbread.com>
2023-01-09 18:14:27 -0800

    Remove configuring flathub due to network requirement

--- home/nixos.nix
+++ home/nixos.nix
@@ -34,12 +34,6 @@ in
       ".librewolf/profile/chrome/firefox-vertical-tabs.css".source =
         pkgs.firefox-vertical-tabs + /userChrome.css;
     };
-    activation.configureFlathub =
-      let flatpak = "${pkgs.flatpak}/bin/flatpak"; in
-      lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        $DRY_RUN_CMD ${flatpak} --user remote-add --if-not-exists $VERBOSE_ARG \
-          flathub https://flathub.org/repo/flathub.flatpakrepo
-      '';
   };
 
   programs = mapAttrs (_: v: v // { enable = true; }) {
