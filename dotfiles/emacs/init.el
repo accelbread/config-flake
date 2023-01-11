bb4e621f74c098aca335f0d68d5448ce84121100
Archit Gupta <archit@accelbread.com>
2023-01-11 01:39:44 -0800

    emacs: Add coterm

--- dotfiles/emacs/init.el
+++ dotfiles/emacs/init.el
@@ -37,7 +37,8 @@
          rmsbolt yasnippet rainbow-mode svg-lib reformatter markdown-mode
          clang-format cmake-mode rust-mode cargo zig-mode nix-mode haskell-mode
          geiser-guile scad-mode toml-mode yaml-mode git-modes pdf-tools
-         flymake-vale meow-term meow-vterm vundo git-annex magit-annex))
+         flymake-vale meow-term meow-vterm vundo git-annex magit-annex
+         coterm))
 
 (setq package-native-compile t)
 
@@ -784,6 +785,83 @@
 
 (advice-add 'pcomplete-completions-at-point :around #'cape-wrap-silent)
 (advice-add 'pcomplete-completions-at-point :around #'cape-wrap-purify)
+
+(coterm-mode)
+
+(defvar-local meow-coterm-char-mode 'auto
+  "Which mode coterm should use in meow insert mode.")
+
+(defun meow-coterm-char-mode ()
+  "Enable coterm char mode."
+  (interactive)
+  (setq meow-coterm-char-mode t)
+  (when (meow-insert-mode-p)
+    (coterm-auto-char-mode -1)
+    (coterm-char-mode)
+    (coterm-scroll-snap-mode))
+  (force-mode-line-update))
+
+(defun meow-coterm-line-mode ()
+  "Enable coterm line mode."
+  (interactive)
+  (setq meow-coterm-char-mode nil)
+  (when (meow-insert-mode-p)
+    (coterm-auto-char-mode -1)
+    (coterm-char-mode -1)
+    (coterm-scroll-snap-mode -1))
+  (force-mode-line-update))
+
+(defun meow-coterm-auto-mode ()
+  "Enable coterm auto mode."
+  (interactive)
+  (setq meow-coterm-char-mode 'auto)
+  (when (meow-insert-mode-p)
+    (coterm-auto-char-mode))
+  (force-mode-line-update))
+
+(defun meow-coterm-insert-enter ()
+  "Switch keybinds to char mode if char mode set."
+  (pcase meow-coterm-char-mode
+    ('auto (coterm-auto-char-mode))
+    ('t (coterm-char-mode)
+        (coterm-scroll-snap-mode)))
+  (force-mode-line-update))
+
+(defun meow-coterm-insert-exit ()
+  "Turn off coterm char mode in meow normal mode."
+  (coterm-auto-char-mode -1)
+  (coterm-char-mode -1)
+  (coterm-scroll-snap-mode -1))
+
+(defun meow-coterm-setup-hooks ()
+  "Ensure non-char-mode keybindings outside of insert mode."
+  (add-hook 'meow-insert-enter-hook #'meow-coterm-insert-enter nil t)
+  (add-hook 'meow-insert-exit-hook #'meow-coterm-insert-exit nil t))
+
+(add-hook 'comint-mode-hook #'meow-coterm-setup-hooks)
+
+(advice-add #'coterm--init :around
+            (lambda (orig-fun &rest args)
+              "Advise `coterm--init' to use disable default auto mode."
+              (cl-letf (((symbol-function #'coterm-auto-char-mode) #'ignore))
+                (apply orig-fun args)))
+            '((name . meow-coterm)))
+
+(with-eval-after-load 'comint
+  (define-key comint-mode-map (kbd "C-c ESC") #'term-send-raw)
+  (define-key comint-mode-map (kbd "C-c C-j") #'meow-coterm-auto-mode)
+  (define-key comint-mode-map (kbd "C-c C-S-j") #'meow-coterm-line-mode)
+  (define-key comint-mode-map (kbd "C-c C-k") #'meow-coterm-char-mode))
+
+(define-key coterm-char-mode-map [remap term-line-mode] #'meow-coterm-auto-mode)
+(define-key coterm-char-mode-map [remap term-char-mode] #'meow-coterm-char-mode)
+
+(put 'coterm-auto-char-lighter-mode-format 'risky-local-variable t)
+(setq coterm-auto-char-lighter-mode-format
+      '(:eval (pcase meow-coterm-char-mode
+                ('auto (when coterm-char-mode " AChar"))
+                ('t " Char")
+                ('nil " Line"))))
 
 
 ;;; Eshell
