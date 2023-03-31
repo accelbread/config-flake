bf4db12b0178223e3c022d5ac422f8f731b83ef9
Archit Gupta <archit@accelbread.com>
2023-03-30 18:28:57 -0700

    pkgs: Replace rec in r8152-udev-rules mkDerivation

--- packages/r8152-udev-rules.nix
+++ packages/r8152-udev-rules.nix
@@ -1,13 +1,13 @@
 { stdenv
 , fetchFromGitHub
 }:
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "r8152-udev-rules";
-  version = "v2.16.3.20221209";
+  version = finalAttrs.src.rev;
   src = fetchFromGitHub {
     owner = "wget";
     repo = "realtek-r8152-linux";
-    rev = version;
+    rev = "v2.16.3.20221209";
     sha256 = "sha256-RaYuprQFbWAy8CtSZOau0Qlo3jtZnE1AhHBgzASopSA=";
   };
   dontBuild = true;
@@ -18,4 +18,4 @@ stdenv.mkDerivation rec {
     cp 50-usb-realtek-net.rules $out/lib/udev/rules.d/
     runHook postInstall
   '';
-}
+})
