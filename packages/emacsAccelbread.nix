39915dbb011a815300212337a170583ee0bb31b8
Archit Gupta <archit@accelbread.com>
2023-04-13 16:45:50 -0700

    emacs: Remove default programs that are project dependent
    Rust-analyzer needs a lot of rust dependencies, zls is likely to need to match
    project, etc. These deps can be gotten from devshell with envrc so the default
    programs should be limited to those that work in a generic context.

--- packages/emacsAccelbread.nix
+++ packages/emacsAccelbread.nix
@@ -1,6 +1,5 @@
 { lib
 , writeText
-, writeShellScript
 , emacsPgtk
 , emacsPackagesFor
 , aspell
@@ -10,13 +9,7 @@
 , direnv
 , fish
 , clang-tools
-, rust-analyzer
-, rustfmt
-, zls
 , nil
-, haskell-language-server
-, ghc
-, stylish-haskell
 , openscad
 , symlinkJoin
 , vale-proselint
@@ -44,17 +37,9 @@ let
     BasedOnStyles = proselint, write-good
     write-good.E-Prime = NO
   '';
-  hls-wrapper = writeShellScript "hls-wrapper" ''
-    export PATH=${haskell-language-server}/bin:${ghc}/bin:$PATH
-    exec ${haskell-language-server}/bin/haskell-language-server-wrapper "$@"
-  '';
   default-init = writeText "default.el" ''
     (setq ispell-program-name "${aspell}/bin/aspell"
           magit-git-executable "${git}/bin/git"
-          clang-format-executable "${clang-tools}/bin/clang-format"
-          rust-rustfmt-bin "${rustfmt}/bin/rustfmt"
-          haskell-mode-stylish-haskell-path
-          "${stylish-haskell}/bin/stylish-haskell"
           flymake-vale-program "${vale}/bin/vale"
           flymake-vale-program-args '("--config=${valeConfig}")
           sh-shellcheck-program "${shellcheck}/bin/shellcheck"
@@ -64,21 +49,10 @@ let
     (with-eval-after-load 'eglot
       (setq eglot-server-programs
             `(((c++-mode c-mode) .
-               ,(eglot-alternatives
-                 '("clangd" "${clang-tools}/bin/clangd")))
-              (rust-mode .
-               ,(eglot-alternatives
-                 '("rust-analyzer" "${rust-analyzer}/bin/rust-analyzer")))
-              (zig-mode .
-               ,(eglot-alternatives
-                 '("zls" "${zls}/bin/zls")))
+               ,(eglot-alternatives '("clangd" "${clang-tools}/bin/clangd")))
               (nix-mode .
-               ,(eglot-alternatives
-                 '("nil" "rnix-lsp" "${nil}/bin/nil")))
-              (haskell-mode .
-               ,(eglot-alternatives
-                 '(("haskell-language-server-wrapper" "--lsp")
-                   ("${hls-wrapper}" "--lsp")))))))
+               ,(eglot-alternatives '("nil" "rnix-lsp" "${nil}/bin/nil")))
+              . ,eglot-server-programs)))
   '';
   baseEmacs = emacsPgtk;
   emacsWithPackages = (emacsPackagesFor baseEmacs).emacsWithPackages;
