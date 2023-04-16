93b46aafd7a53285777be4d06bcbb12b3530f5e5
Archit Gupta <archit@accelbread.com>
2023-04-15 21:43:37 -0700

    templates/quickshell: New external flake direnv template

--- templates/default.nix
+++ templates/default.nix
@@ -1,23 +1,10 @@
+{ lib }: lib.mapAttrs (n: v: { path = ./${n}; } // v)
 rec {
-  default = shell;
-  shell = {
-    path = ./shell;
-    description = "Template Nix devShell.";
-  };
-  elisp-package = {
-    path = ./elisp-package;
-    description = "Template Emacs package.";
-  };
-  c-bin = {
-    path = ./c-bin;
-    description = "Template C application.";
-  };
-  rust-bin = {
-    path = ./rust-bin;
-    description = "Template Rust application.";
-  };
-  zig-bin = {
-    path = ./zig-bin;
-    description = "Template Zig application.";
-  };
+  default = shell // { path = ./shell; };
+  shell.description = "Template Nix devShell.";
+  quickshell.description = "Envrc pointing to self flake.";
+  elisp-package.description = "Template Emacs package.";
+  c-bin.description = "Template C application.";
+  rust-bin.description = "Template Rust application.";
+  zig-bin.description = "Template Zig application.";
 }
