f3ca4209b495bb46612aff751d09ac3f1279e72a
Archit Gupta <archit@accelbread.com>
2023-04-15 21:10:08 -0700

    templates/shell: Use flakelite

--- templates/shell/flake.nix
+++ templates/shell/flake.nix
@@ -1,18 +1,7 @@
 {
-  outputs = { self, nixpkgs }:
-    let
-      systems = [ "x86_64-linux" "aarch64-linux" ];
-      eachSystem = with nixpkgs.lib; f: foldAttrs mergeAttrs { }
-        (map (s: mapAttrs (_: v: { ${s} = v; }) (f s)) systems);
-    in
-    eachSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [ ];
-        };
-        formatter = pkgs.nixpkgs-fmt;
-      });
+  outputs = { self, nixpkgs, flakelite }@inputs:
+    flakelite.lib.mkFlake ./. {
+      inherit inputs;
+      devTools = pkgs: with pkgs; [ ];
+    };
 }
