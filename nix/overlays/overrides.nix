e17d5ee690e3c2f65fc22e2ef07bddaf01f11aa5
Archit Gupta <archit@accelbread.com>
2023-05-21 14:23:10 -0700

    nixos: Remove gnomeExtensions.espresso override

--- nix/overlays/overrides.nix
+++ nix/overlays/overrides.nix
@@ -10,37 +10,4 @@ final: prev: {
       "--with-statepath=/var/lib/nut"
     ];
   });
-  gnomeExtensions = prev.gnomeExtensions // {
-    "espresso" =
-      let
-        uuid = "espresso@coadmunkee.github.com";
-      in
-      final.stdenv.mkDerivation rec {
-        pname = "gnome-shell-extension-espresso";
-        version = "git";
-        src = prev.fetchFromGitHub {
-          owner = "coadmunkee";
-          repo = "gnome-shell-extension-espresso";
-          rev = "8dfb8df2671fcf2052c0cb4533dd5de783a00afe";
-          sha256 = "sha256-6VIXeCFFXQxqrfLQuPOZli8h8vITiTKdhE+tQg/J3Os=";
-        };
-        nativeBuildInputs = with final; [ gettext glib ];
-        buildPhase = ''
-          ${final.runtimeShell} ./update-locale.sh
-          glib-compile-schemas --strict \
-            --targetdir=espresso@coadmunkee.github.com/schemas/ \
-            espresso@coadmunkee.github.com/schemas
-        '';
-        installPhase = ''
-          runHook preInstall
-          mkdir -p $out/share/gnome-shell/extensions
-          cp -r ${uuid} $out/share/gnome-shell/extensions/
-          runHook postInstall
-        '';
-        passthru = {
-          extensionPortalSlug = pname;
-          extensionUuid = uuid;
-        };
-      };
-  };
 }
