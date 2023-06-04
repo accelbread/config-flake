a0912caa8a62998fb0357e7c68e82d5d725cda2f
Archit Gupta <archit@accelbread.com>
2023-06-04 11:03:12 -0700

    flake: Update inputs

--- nix/packages/emacsAccelbread.nix
+++ nix/packages/emacsAccelbread.nix
@@ -1,6 +1,6 @@
 { lib
 , writeText
-, emacsPgtk
+, emacs-pgtk
 , emacsPackagesFor
 , git
 , vale
@@ -51,10 +51,7 @@ let
                ,(eglot-alternatives '("nil" "rnix-lsp" "${nil}/bin/nil")))
               . ,eglot-server-programs)))
   '';
-  baseEmacs = emacsPgtk.overrideAttrs (old: {
-    patches = (old.patches or [ ]) ++
-      [ ./misc/0001-Revert-Better-compilation-of-arguments-to-ignore.patch ];
-  });
+  baseEmacs = emacs-pgtk;
   emacsWithPackages = (emacsPackagesFor baseEmacs).emacsWithPackages;
 in
 emacsWithPackages (epkgs: attrVals configPackages epkgs
