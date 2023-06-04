dc6609f66d65649e3738b2e4d02beec9c29138d4
Archit Gupta <archit@accelbread.com>
2023-06-04 12:03:08 -0700

    usbguard-dbus: Clean up group permissions

--- nix/nixosModules/usbguard-dbus.nix
+++ nix/nixosModules/usbguard-dbus.nix
@@ -4,16 +4,7 @@ let
   cfg = config.services.usbguard;
 in
 {
-  options.services.usbguard.dbus = {
-    enable = mkEnableOption "USBGuard dbus daemon";
-    group = mkOption {
-      type = types.str;
-      default = head cfg.IPCAllowedGroups;
-      description = ''
-        Which group to use to allow access to the USBGuard dbus service.
-      '';
-    };
-  };
+  options.services.usbguard.dbus.enable = mkEnableOption "USBGuard dbus daemon";
 
   config = mkIf (cfg.enable && cfg.dbus.enable) {
     systemd.services.usbguard-dbus = {
@@ -29,20 +20,26 @@ in
       aliases = [ "dbus-org.usbguard.service" ];
     };
 
-    security.polkit.extraConfig = ''
-      polkit.addRule(function(action, subject) {
-          if ((action.id == "org.usbguard.Policy1.listRules" ||
-               action.id == "org.usbguard.Policy1.appendRule" ||
-               action.id == "org.usbguard.Policy1.removeRule" ||
-               action.id == "org.usbguard.Devices1.applyDevicePolicy" ||
-               action.id == "org.usbguard.Devices1.listDevices" ||
-               action.id == "org.usbguard1.getParameter" ||
-               action.id == "org.usbguard1.setParameter") &&
-              subject.active == true && subject.local == true &&
-              subject.isInGroup("${cfg.dbus.group}")) {
-                  return polkit.Result.YES;
-          }
-      });
-    '';
+    security.polkit.extraConfig =
+      let
+        groupCheck = "(" + (lib.concatStrings
+          (map (g: "subject.isInGroup(\"${g}\") || ") cfg.IPCAllowedGroups))
+          + "false)";
+      in
+      ''
+        polkit.addRule(function(action, subject) {
+            if ((action.id == "org.usbguard.Policy1.listRules" ||
+                 action.id == "org.usbguard.Policy1.appendRule" ||
+                 action.id == "org.usbguard.Policy1.removeRule" ||
+                 action.id == "org.usbguard.Devices1.applyDevicePolicy" ||
+                 action.id == "org.usbguard.Devices1.listDevices" ||
+                 action.id == "org.usbguard1.getParameter" ||
+                 action.id == "org.usbguard1.setParameter") &&
+                subject.active == true && subject.local == true &&
+                ${groupCheck}) {
+                    return polkit.Result.YES;
+            }
+        });
+      '';
   };
 }
