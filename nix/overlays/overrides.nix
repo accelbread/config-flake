f9234ca1a6236f039b7ab29596c41dba53bb59d4
Archit Gupta <accelbread@gmail.com>
2023-06-22 21:05:29 -0700

    nixos: Fix LKRG's systemd-coredump path

--- nix/overlays/overrides.nix
+++ nix/overlays/overrides.nix
@@ -19,28 +19,32 @@ final: prev: {
     '';
   });
   linuxPackages_hardened = prev.linuxPackages_hardened.extend (_: lprev: {
-    lkrg = lprev.lkrg.overrideAttrs (old: rec {
-      version = "0.9.6";
-      src = prev.fetchFromGitHub {
-        owner = "lkrg-org";
-        repo = "lkrg";
-        rev = "v${version}";
-        sha256 = "sha256-jKiSTab05+6ZZXQDKUVPKGti0E4eZaVuMZJlBKR3zGY=";
-      };
-      patches = [ ];
-      meta = old.meta // { broken = false; };
-      prePatch = old.prePatch + ''
-        substituteInPlace src/modules/exploit_detection/syscalls/p_call_usermodehelper/p_call_usermodehelper.c \
-          --replace \"/bin/false \"${final.coreutils}/bin/false\",\"/run/current-system/sw/bin/false \
-          --replace \"/bin/true \"${final.coreutils}/bin/true\",\"/run/current-system/sw/bin/true \
-          --replace \"/lib/systemd/systemd-cgroups-agent \"${final.systemd}/lib/systemd/systemd-cgroups-agent \
-          --replace \"/lib/systemd/systemd-coredump \"${final.systemd}/lib/systemd/systemd-coredump \
-          --replace \"/sbin/bridge-stp \"/run/current-system/sw/bin/bridge-stp \
-          --replace \"/sbin/drbdadm \"/run/current-system/sw/bin/drbdadm \
-          --replace \"/sbin/modprobe \"${final.kmod}/bin/modprobe \
-          --replace \"/sbin/poweroff \"${final.systemd}/sbin/poweroff \
-          --replace \"/sbin/request-key \"/run/current-system/sw/bin/request-key \
-      '';
-    });
+    lkrg = lprev.lkrg.overrideAttrs (old:
+      let
+        systemd-coredump-pkg = final.symlinkJoin { name = "systemd"; paths = [ final.systemd ]; };
+      in
+      rec {
+        version = "0.9.6";
+        src = prev.fetchFromGitHub {
+          owner = "lkrg-org";
+          repo = "lkrg";
+          rev = "v${version}";
+          sha256 = "sha256-jKiSTab05+6ZZXQDKUVPKGti0E4eZaVuMZJlBKR3zGY=";
+        };
+        patches = [ ];
+        meta = old.meta // { broken = false; };
+        prePatch = old.prePatch + ''
+          substituteInPlace src/modules/exploit_detection/syscalls/p_call_usermodehelper/p_call_usermodehelper.c \
+            --replace \"/bin/false \"${final.coreutils}/bin/false\",\"/run/current-system/sw/bin/false \
+            --replace \"/bin/true \"${final.coreutils}/bin/true\",\"/run/current-system/sw/bin/true \
+            --replace \"/lib/systemd/systemd-cgroups-agent \"${final.systemd}/lib/systemd/systemd-cgroups-agent \
+            --replace \"/lib/systemd/systemd-coredump \"${systemd-coredump-pkg}/lib/systemd/systemd-coredump \
+            --replace \"/sbin/bridge-stp \"/run/current-system/sw/bin/bridge-stp \
+            --replace \"/sbin/drbdadm \"/run/current-system/sw/bin/drbdadm \
+            --replace \"/sbin/modprobe \"${final.kmod}/bin/modprobe \
+            --replace \"/sbin/poweroff \"${final.systemd}/sbin/poweroff \
+            --replace \"/sbin/request-key \"/run/current-system/sw/bin/request-key \
+        '';
+      });
   });
 }
