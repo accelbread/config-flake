226f0d1752550b44848e7400239a344e3a57455a
Archit Gupta <archit@accelbread.com>
2023-10-15 21:01:34 -0700

    emacs: Map c-or-c++-mode to c-or-c++-ts-mode

--- dotfiles/emacs/init.el
+++ dotfiles/emacs/init.el
@@ -1221,6 +1221,7 @@
 
 (dolist (item '((c-mode . c-ts-mode)
                 (c++-mode . c++-ts-mode)
+                (c-or-c++-mode . c-or-c++-ts-mode)
                 (cmake-mode . cmake-ts-mode)
                 (rust-mode . rust-ts-mode)
                 (python-mode . python-ts-mode)
