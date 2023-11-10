41f7037be047faad6e7443786fea94590a9a6961
Archit Gupta <archit@accelbread.com>
2023-11-09 19:55:57 -0800

    emacs: Bundle required fonts

--- nix/packages/emacsAccelbread.nix
+++ nix/packages/emacsAccelbread.nix
@@ -13,9 +13,20 @@
 , symlinkJoin
 , vale-proselint
 , vale-write-good
+, makeFontsConf
+, dejavu_fonts
+, noto-fonts
+, noto-fonts-extra
+, noto-fonts-cjk-sans
+, noto-fonts-cjk-serif
+, noto-fonts-color-emoji
+, noto-fonts-monochrome-emoji
+, runCommand
+, makeBinaryWrapper
 }:
 let
   inherit (lib) pipe attrVals;
+
   configPackages = pipe (../../dotfiles/emacs/init.el) (with builtins; [
     readFile
     (match ".*\\(setopt package-selected-packages[[:space:]]+'\\(([^)]+).*")
@@ -24,10 +35,12 @@ let
     (filter isList)
     (map head)
   ]);
+
   valeStyles = symlinkJoin {
     name = "vale-styles";
     paths = [ vale-proselint vale-write-good ];
   };
+
   valeConfig = writeText "vale-config" ''
     StylesPath = ${valeStyles}
     MinAlertLevel = suggestion
@@ -35,6 +48,19 @@ let
     BasedOnStyles = proselint, write-good
     write-good.E-Prime = NO
   '';
+
+  fontConfig = makeFontsConf {
+    fontDirectories = [
+      dejavu_fonts
+      noto-fonts
+      noto-fonts-extra
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      noto-fonts-color-emoji
+      noto-fonts-monochrome-emoji
+    ];
+  };
+
   default-init = writeText "default.el" ''
     (setq magit-git-executable "${git}/bin/git"
           flymake-vale-program "${vale}/bin/vale"
@@ -51,27 +77,45 @@ let
                ,(eglot-alternatives '("nil" "rnix-lsp" "${nil}/bin/nil")))
               . ,eglot-server-programs)))
   '';
+
   baseEmacs = emacs29-pgtk;
+
   inherit (emacsPackagesFor baseEmacs) emacsWithPackages;
+
+  emacsWPkgs = emacsWithPackages (epkgs: attrVals configPackages epkgs ++ [
+    (epkgs.treesit-grammars.with-grammars (grammars: with grammars; [
+      tree-sitter-c
+      tree-sitter-cpp
+      tree-sitter-cmake
+      tree-sitter-rust
+      tree-sitter-python
+      tree-sitter-java
+      tree-sitter-json
+      tree-sitter-toml
+      tree-sitter-yaml
+      tree-sitter-html
+      tree-sitter-css
+      tree-sitter-javascript
+    ]))
+    (epkgs.trivialBuild {
+      pname = "emacs-default-init";
+      version = "0.0.1";
+      src = default-init;
+    })
+  ]);
+
+  wrapEmacs = emacs: runCommand emacs.name
+    {
+      nativeBuildInputs = [ makeBinaryWrapper ];
+      inherit (emacs) meta;
+    }
+    ''
+      mkdir -p $out/bin
+      for bin in ${emacs}/bin/*; do
+        makeWrapper "$bin" $out/bin/$(basename "$bin") --inherit-argv0 \
+          --set FONTCONFIG_FILE ${fontConfig}
+      done
+      ln -s ${emacs}/share $out/share
+    '';
 in
-emacsWithPackages (epkgs: attrVals configPackages epkgs ++ [
-  (epkgs.treesit-grammars.with-grammars (grammars: with grammars; [
-    tree-sitter-c
-    tree-sitter-cpp
-    tree-sitter-cmake
-    tree-sitter-rust
-    tree-sitter-python
-    tree-sitter-java
-    tree-sitter-json
-    tree-sitter-toml
-    tree-sitter-yaml
-    tree-sitter-html
-    tree-sitter-css
-    tree-sitter-javascript
-  ]))
-  (epkgs.trivialBuild {
-    pname = "emacs-default-init";
-    version = "0.0.1";
-    src = default-init;
-  })
-])
+wrapEmacs emacsWPkgs
