923b7607b4165e1c3c45439fc331c64903dabbca
Archit Gupta <archit@accelbread.com>
2023-11-19 19:50:55 -0800

    shadowfang: Set default audio input device

--- nix/nixos/shadowfang/home.nix
+++ nix/nixos/shadowfang/home.nix
@@ -1,4 +1,4 @@
-{
+{ lib, ... }: {
   dconf.settings = {
     "org/gnome/desktop/peripherals/touchpad" = {
       speed = 0.4;
@@ -12,4 +12,18 @@
       sleep-inactive-battery-type = "hibernate";
     };
   };
+  home.activation.wireplumberDefaults =
+    let
+      defaultNodes = builtins.toFile "wireplumber-default-nodes" ''
+        [default-nodes]
+        default.configured.audio.source=rnnoise_source
+        default.configured.audio.source.0=rnnoise_source
+        default.configured.audio.source.1=alsa_input.pci-0000_00_1f.3.analog-stereo
+      '';
+    in
+    lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      $DRY_RUN_CMD mkdir -p "$HOME/.local/state/wireplumber"
+      $DRY_RUN_CMD cat ${defaultNodes} \
+        > "$HOME/.local/state/wireplumber/default-nodes"
+    '';
 }
