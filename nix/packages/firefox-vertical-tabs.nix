a62bf31bdaa14a5240fdf48ca83b00a448749d77
Archit Gupta <archit@accelbread.com>
2023-11-23 15:43:57 -0800

    packages: Update package versions

--- nix/packages/firefox-vertical-tabs.nix
+++ nix/packages/firefox-vertical-tabs.nix
@@ -2,6 +2,6 @@
 fetchFromGitHub {
   owner = "ranmaru22";
   repo = "firefox-vertical-tabs";
-  rev = "v5.9";
-  sha256 = "sha256-x0fLq1O108XvRgFcmedJy/HZQrMPOmjsDWMwT5yjoNo=";
+  rev = "v6.1";
+  sha256 = "sha256-mr4Sh9iZRs/AI+6DzuUV8E75mE3KiCSanglGi/K/a4M=";
 }
--- nix/packages/jdt-language-server.nix
+++ nix/packages/jdt-language-server.nix
@@ -5,12 +5,12 @@
 }:
 stdenv.mkDerivation rec {
   pname = "jdt-language-server";
-  version = "1.28.0";
-  timestamp = "202309281329";
+  version = "1.29.0";
+  timestamp = "202310261436";
 
   src = fetchzip {
     url = "https://download.eclipse.org/jdtls/milestones/${version}/jdt-language-server-${version}-${timestamp}.tar.gz";
-    sha256 = "sha256-Kz8+e/VKc5DeG3SZAn7cuKA0XaAygEHhXN4SvbQTVxQ=";
+    sha256 = "sha256-ikrmrih0jI4XjdE/Dh+HeAtCMKmRHFuB2b8JI2i7nwE=";
     stripRoot = false;
   };
 
