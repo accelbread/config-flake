f135ba73f5a8ab97384629de6773c746675f41e0
Archit Gupta <archit@accelbread.com>
2023-12-17 20:04:36 -0800

    esphome: Use systemd timers for bulb automation

--- nix/nixos/solace/mqtt-broker.nix
+++ nix/nixos/solace/mqtt-broker.nix
@@ -1,4 +1,4 @@
-{
+{ pkgs, ... }: {
   services.mosquitto = {
     enable = true;
     persistence = false;
@@ -12,4 +12,34 @@
   networking.firewall.extraInputRules = ''
     ip saddr 192.168.0.0/16 tcp dport 1883 accept
   '';
+  systemd = {
+    services."light-temp" = {
+      wantedBy = [ "mosquitto.service" ];
+      requisite = [ "mosquitto.service" ];
+      after = [ "mosquitto.service" ];
+      serviceConfig.Type = "oneshot";
+      path = with pkgs; [ coreutils mqttui ];
+      script = ''
+        set -eu
+        curr_time=$(date +%H:%M)
+        if [[ "$curr_time" > "08:00" ]] && [[ "$curr_time" < "20:00" ]]; then
+          bulb_msg='{"color_temp":150,"brightness":256}'
+        else
+          bulb_msg='{"color_temp":300,"brightness":220}'
+        fi
+        for bulb in kauf-bulb-df30ae kauf-bulb-df31f3; do
+          mqttui publish -r "home/bulb/$bulb/light/kauf_bulb/command" "$bulb_msg"
+        done
+      '';
+    };
+    timers."light-temp" = {
+      wantedBy = [ "mosquitto.service" ];
+      requisite = [ "mosquitto.service" ];
+      after = [ "mosquitto.service" ];
+      timerConfig = {
+        OnCalendar = [ "*-*-* 8:00:00" "*-*-* 20:00:00" ];
+        Persistent = true;
+      };
+    };
+  };
 }
