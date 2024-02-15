21df87bb7436753c161f6b9eff95f411ea6f6a2f
Archit Gupta <archit@accelbread.com>
2024-02-14 23:13:16 -0800

    nixos: Apply ollama service hardening

--- nix/nixos/solace/ollama.nix
+++ nix/nixos/solace/ollama.nix
@@ -9,4 +9,39 @@
   ];
 
   networking.firewall.interfaces."tailscale0".allowedTCPPorts = [ 11434 ];
+
+  systemd.services.ollama.serviceConfig = {
+    UMask = 077;
+    CapabilityBoundingSet = "";
+    DeviceAllow = [
+      "/dev/dri/card0 rw"
+      "/dev/dri/renderD128 rw"
+      "/dev/kfd rw"
+    ];
+    DevicePolicy = "closed";
+    LockPersonality = true;
+    MemoryDenyWriteExecute = true;
+    NoNewPrivileges = true;
+    PrivateIPC = true;
+    PrivateMounts = true;
+    PrivateTmp = true;
+    PrivateUsers = true;
+    ProcSubset = "pid";
+    ProtectClock = true;
+    ProtectControlGroups = true;
+    ProtectHome = true;
+    ProtectHostname = true;
+    ProtectKernelLogs = true;
+    ProtectKernelModules = true;
+    ProtectKernelTunables = true;
+    ProtectProc = "invisible";
+    ProtectSystem = "strict";
+    RemoveIPC = true;
+    RestrictAddressFamilies = [ "AF_INET" ];
+    RestrictNamespaces = true;
+    RestrictRealtime = true;
+    RestrictSUIDSGID = true;
+    SystemCallArchitectures = "native";
+    SystemCallFilter = [ "@system-service" "~@privileged" ];
+  };
 }
