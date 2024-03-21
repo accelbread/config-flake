d69a8b2d636b322fa1e8ba853bfbf23f9a858e38
Archit Gupta <archit@accelbread.com>
2024-03-21 13:03:41 -0700

    emacs: Remove cape-wrap-noninterruptible from eglot capf
    This has been fixed upstream in jsonrpc.el commit
    5a0250f76676d8750f42bbca1af5c4553846949b.

--- dotfiles/emacs/init.el
+++ dotfiles/emacs/init.el
@@ -1186,7 +1186,6 @@
 
 (push '(eglot (styles orderless)) completion-category-overrides)
 
-(advice-add #'eglot-completion-at-point :around #'cape-wrap-noninterruptible)
 (advice-add #'eglot-completion-at-point :around #'cape-wrap-nonexclusive)
 
 (with-eval-after-load 'yasnippet
