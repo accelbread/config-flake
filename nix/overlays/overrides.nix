5dfc8fa2afa787bd2665d3dbec28fbbb4d9b62bb
Archit Gupta <archit@accelbread.com>
2024-03-31 12:47:23 -0700

    nixos: Mount bees workdir with noatime

--- nix/overlays/overrides.nix
+++ nix/overlays/overrides.nix
@@ -11,4 +11,15 @@ final: prev: {
     ];
   });
   tpm2-pkcs11 = prev.tpm2-pkcs11.override { fapiSupport = false; };
+  bees = prev.bees.overrideAttrs {
+    utillinux = final.runCommand final.util-linux.name
+      {
+        inherit (final.util-linux) meta pname version;
+        nativeBuildInputs = [ final.makeBinaryWrapper ];
+      } ''
+      cp -r ${final.util-linux} $out
+      chmod -R u+w $out
+      wrapProgram $out/bin/mount --add-flags "-o noatime"
+    '';
+  };
 }
