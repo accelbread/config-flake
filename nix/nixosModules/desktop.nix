74e65c975331fb320b625a1420c0effc9d57268f
Archit Gupta <archit@accelbread.com>
2024-08-26 20:57:39 -0700

    desktop: Add gstreamer plugins

--- nix/nixosModules/desktop.nix
+++ nix/nixosModules/desktop.nix
@@ -91,6 +91,12 @@
   xdg.portal.xdgOpenUsePortal = true;
 
   environment = {
+    sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 =
+      lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" [
+        pkgs.gst_all_1.gst-plugins-base
+        pkgs.gst_all_1.gst-plugins-good
+        pkgs.gst_all_1.gst-libav
+      ];
     systemPackages = [ pkgs.gnome-themes-extra ];
     persistence = {
       "/persist/state".users = {
