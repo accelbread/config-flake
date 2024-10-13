352710663d56bb3db89727c56b98e96ec8d8f7e0
Archit Gupta <archit@accelbread.com>
2024-10-13 11:05:14 -0700

    flake: Remove unused packages

--- nix/packages/firefox-vertical-tabs.nix
+++
@@ -1,7 +0,0 @@
-{ fetchFromGitHub }:
-fetchFromGitHub {
-  owner = "ranmaru22";
-  repo = "firefox-vertical-tabs";
-  rev = "v6.1";
-  sha256 = "sha256-mr4Sh9iZRs/AI+6DzuUV8E75mE3KiCSanglGi/K/a4M=";
-}
