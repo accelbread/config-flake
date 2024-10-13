ee088f517c3e708987397c53a8172329c518cba3
Archit Gupta <archit@accelbread.com>
2024-10-13 13:06:45 -0700

    flake: Switch to nixos-unstable-small

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     flakelight = {
       url = "github:nix-community/flakelight";
       inputs.nixpkgs.follows = "nixpkgs";
