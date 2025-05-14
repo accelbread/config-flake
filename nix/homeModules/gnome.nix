a5c7042b7fd035c535f7961106d698b5cff1ac03
Archit Gupta <archit@accelbread.com>
2025-05-13 17:11:00 -0700

    flake: Update inputs
    Flake lock file updates:
    
    • Updated input 'emacs-overlay':
        'github:nix-community/emacs-overlay/24a3e2b6b0dda50c1afaa3da730f156d0400e95a' (2025-04-27)
      → 'github:nix-community/emacs-overlay/aa9f9769d54ed2d0e062755a82485f99d90f3d5f' (2025-05-13)
    • Updated input 'emacs-overlay/nixpkgs-stable':
        'github:NixOS/nixpkgs/5630cf13cceac06cefe9fc607e8dfa8fb342dde3' (2025-04-24)
      → 'github:NixOS/nixpkgs/a39ed32a651fdee6842ec930761e31d1f242cb94' (2025-05-11)
    • Updated input 'flakelight':
        'github:nix-community/flakelight/9c4ffe2b9e848fb8ae6b51a3ee71c5f3055109d9' (2025-04-23)
      → 'github:nix-community/flakelight/95f19ef3204e166de16149008e7667000cbe8f23' (2025-05-12)
    • Updated input 'flakelight-elisp':
        'github:accelbread/flakelight-elisp/24edcc2af55925193253badd72a94e3fe732db91' (2025-04-22)
      → 'github:accelbread/flakelight-elisp/e2c24e530392c8aa5b882b05c88ee4ac6d38c18d' (2025-05-13)
    • Updated input 'home-manager':
        'github:nix-community/home-manager/9c46dc881c2afcb50ac9ae9f1c36b2a4ebba3c8a' (2025-04-28)
      → 'github:nix-community/home-manager/b44c39cf4618ccf58df2b68827ca95a3fecf0655' (2025-05-14)
    • Updated input 'nixos-hardware':
        'github:NixOS/nixos-hardware/f7bee55a5e551bd8e7b5b82c9bc559bc50d868d1' (2025-04-24)
      → 'github:NixOS/nixos-hardware/e81fd167b33121269149c57806599045fd33eeed' (2025-05-13)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/a8cbaf7900c1b5e9becb4e00b22c4044ea12f6bc' (2025-04-27)
      → 'github:NixOS/nixpkgs/fab95ba4b9523f310644e6e6087c0014535c8e02' (2025-05-13)

--- nix/homeModules/gnome.nix
+++ nix/homeModules/gnome.nix
@@ -15,7 +15,7 @@
 
   services.gpg-agent = {
     enable = true;
-    pinentryPackage = pkgs.pinentry-gnome3;
+    pinentry.package = pkgs.pinentry-gnome3;
     defaultCacheTtl = 300;
     maxCacheTtl = 1800;
     extraConfig = ''
