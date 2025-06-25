f7c52f76ff7746f418a6c7d0714c32d57ec2e171
Archit Gupta <archit@accelbread.com>
2025-06-25 12:24:55 -0700

    emacs: Revert stipple usage

--- dotfiles/emacs/init.el
+++ dotfiles/emacs/init.el
@@ -253,7 +253,6 @@
         whitespace-style '(face trailing tab-mark tabs missing-newline-at-eof)
         whitespace-global-modes '(prog-mode text-mode conf-mode)
         global-display-fill-column-indicator-modes '(prog-mode text-mode)
-        display-fill-column-indicator-character ?\s
         resize-mini-windows t
         suggest-key-bindings nil
         truncate-partial-width-windows 83
@@ -270,9 +269,6 @@
 (global-prettify-symbols-mode)
 (global-hl-todo-mode)
 (context-menu-mode)
-
-(set-face-attribute 'fill-column-indicator nil :stipple
-                    `(,(string-pixel-width " ") 1 "\x8\x0"))
 
 (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
 (add-hook 'prog-mode-hook #'visual-wrap-prefix-mode)
