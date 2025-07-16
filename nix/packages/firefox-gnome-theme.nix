ab43115a8b7ff0f01a47dd2dacb175f69970539f
Archit Gupta <archit@accelbread.com>
2025-07-15 22:09:13 -0700

    packages/firefox-gnome-theme: Update to v140

--- nix/packages/firefox-gnome-theme.nix
+++ nix/packages/firefox-gnome-theme.nix
@@ -2,6 +2,6 @@
 fetchFromGitHub {
   owner = "rafaelmardojai";
   repo = "firefox-gnome-theme";
-  rev = "4eb2714fbed2b80e234312611a947d6cb7d70caf";
-  sha256 = "sha256-pGvD/RGuuPf/4oogsfeRaeMm6ipUIznI2QSILKjKzeA=";
+  rev = "v140";
+  hash = "sha256-/pTDAny47X1og2pSAoMY/GfJ9teSOEQZAENhszPJMKo=";
 }
