a56f4b6239d1fde4b252eaaefa311255ce54ca12
Archit Gupta <archit@accelbread.com>
2025-10-02 11:43:07 -0700

    packages/firefox-gnome-theme: Update to v143

--- nix/packages/firefox-gnome-theme.nix
+++ nix/packages/firefox-gnome-theme.nix
@@ -2,6 +2,6 @@
 fetchFromGitHub {
   owner = "rafaelmardojai";
   repo = "firefox-gnome-theme";
-  rev = "b655eaf16d4cbec9c3472f62eee285d4b419a808";
-  hash = "sha256-UqYGTBgI5ypGh0Kf6zZjom/vABg7HQocB4gmxzl12uo=";
+  rev = "v143";
+  hash = "sha256-0E3TqvXAy81qeM/jZXWWOTZ14Hs1RT7o78UyZM+Jbr4=";
 }
