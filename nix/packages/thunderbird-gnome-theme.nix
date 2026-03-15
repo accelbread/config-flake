b1a336725c517a8c2542beaceec17f0b2bb3e370
Archit Gupta <archit@accelbread.com>
2026-03-14 20:37:40 -0700

    packages/thunderbird-gnome-theme: Update to master

--- nix/packages/thunderbird-gnome-theme.nix
+++ nix/packages/thunderbird-gnome-theme.nix
@@ -2,6 +2,6 @@
 fetchFromGitHub {
   owner = "rafaelmardojai";
   repo = "thunderbird-gnome-theme";
-  rev = "167a451201feb5cf5681106f84c1b9f4206d8ed7";
-  hash = "sha256-Qj44FyE50ZdL5oxSCf65NgEh0Telz1QjzlsLgXlREEw=";
+  rev = "b266310c68483fe4c28d38874931cfb2e5f58035";
+  hash = "sha256-nSTxAMH+uGrjMWFv1EKhVOnL6QXmbvJByvtSkMJWeVU=";
 }
