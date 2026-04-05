796ae11bc9c186a7400a181014c65fcc04071caa
Archit Gupta <archit@accelbread.com>
2026-04-05 14:06:40 -0700

    pkgs/{firefox,thunderbird}-gnome-theme: Update to master

--- nix/packages/firefox-gnome-theme.nix
+++ nix/packages/firefox-gnome-theme.nix
@@ -2,6 +2,6 @@
 fetchFromGitHub {
   owner = "rafaelmardojai";
   repo = "firefox-gnome-theme";
-  rev = "43bc26501a637c68df723645966b77168cefa9d0";
-  hash = "sha256-6cGDN/2iSNUJWp035zzr6BZCzFtO92PIKNeWxuwPPcA=";
+  rev = "9a7666e8020d2b8510cac824a9de38cfcf65a567";
+  hash = "sha256-vJN+wriJPyNkOWyrD8cWecWoNPuRLw7dmBpbQ1pAyUU=";
 }
--- nix/packages/thunderbird-gnome-theme.nix
+++ nix/packages/thunderbird-gnome-theme.nix
@@ -2,6 +2,6 @@
 fetchFromGitHub {
   owner = "rafaelmardojai";
   repo = "thunderbird-gnome-theme";
-  rev = "b266310c68483fe4c28d38874931cfb2e5f58035";
-  hash = "sha256-nSTxAMH+uGrjMWFv1EKhVOnL6QXmbvJByvtSkMJWeVU=";
+  rev = "c61b897554b5631c042c5e3c4b76cc021a9890b7";
+  hash = "sha256-BB0V/tvTBEPIpS+UKzQUUjQIRuHxLVAxF+anZW4vH5c=";
 }
