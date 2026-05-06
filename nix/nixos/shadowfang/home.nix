b4307f6360d3f5b22891c0e19f8cda4975de10ec
Archit Gupta <archit@accelbread.com>
2026-05-06 01:25:48 -0700

    shadowfang: Set XM6 as default audio source/sink

--- nix/nixos/shadowfang/home.nix
+++ nix/nixos/shadowfang/home.nix
@@ -5,8 +5,12 @@
     let
       defaultNodes = builtins.toFile "wireplumber-default-nodes" ''
         [default-nodes]
-        default.configured.audio.source=alsa_input.pci-0000_00_1f.3.analog-stereo
-        default.configured.audio.source.0=alsa_input.pci-0000_00_1f.3.analog-stereo
+        default.configured.audio.source=bluez_input.58_18_62_1E_D8_3B.0
+        default.configured.audio.source.0=bluez_input.58_18_62_1E_D8_3B.0
+        default.configured.audio.source.1=alsa_input.pci-0000_00_1f.3.analog-stereo
+        default.configured.audio.sink=bluez_output.58_18_62_1E_D8_3B.1
+        default.configured.audio.sink.0=bluez_output.58_18_62_1E_D8_3B.1
+        default.configured.audio.sink.1=alsa_output.pci-0000_00_1f.3.analog-stereo
       '';
     in
     lib.hm.dag.entryAfter [ "writeBoundary" ] ''
