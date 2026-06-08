ab80242b2a39a90bf34907e913c2734993191541
Archit Gupta <archit@accelbread.com>
2026-06-07 19:17:20 -0700

    desktop: Set global /etc/xdg/monitors.xml

--- nix/nixosModules/monitors.nix
+++ nix/nixosModules/monitors.nix
@@ -11,17 +11,6 @@ in
   };
 
   config = lib.mkIf (monitors != null) {
-    systemd.tmpfiles.rules = [
-      "L+ /run/gdm/.config/monitors.xml - gdm gdm - ${monitors}"
-    ];
-    home-manager.sharedModules = [
-      ({ lib, ... }: {
-        home.activation.monitorsDefaults =
-          lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-            $DRY_RUN_CMD mkdir -p "$HOME/.config"
-            $DRY_RUN_CMD cat ${monitors} > "$HOME/.config/monitors.xml"
-          '';
-      })
-    ];
+    environment.etc."xdg/monitors.xml".source = monitors;
   };
 }
