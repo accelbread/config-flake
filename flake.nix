325a04972309998195b5d686fc9bf3bf11210d40
Archit Gupta <archit@accelbread.com>
2026-06-28 22:41:26 -0700

    nixos: Update lanzaboote

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
     };
     preservation.url = "github:nix-community/preservation";
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.4.3";
+      url = "github:nix-community/lanzaboote/v1.1.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     emacs-overlay = {
