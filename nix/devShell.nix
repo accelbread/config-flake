bca29b1171ea6633b8cc9b951b62873e72244c95
Archit Gupta <archit@accelbread.com>
2026-09-04 22:50:08 -0700

    devShell: Reduce size of default devshell

--- nix/devShell.nix
+++ nix/devShell.nix
@@ -1,5 +1,6 @@
 # Copyright (C) Archit Gupta <archit@accelbread.com>
 # SPDX-License-Identifier: AGPL-3.0-or-later
 pkgs: with pkgs; {
-  packages = [ esphome mqttui b4 reuse ];
+  stdenv = pkgs.stdenvNoCC;
+  packages = [ mqttui b4 ];
 }
