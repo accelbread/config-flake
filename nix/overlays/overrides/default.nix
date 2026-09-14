ae81870add57ca81a86a6c397c69f67bd2f560e5
Archit Gupta <archit@accelbread.com>
2026-09-14 02:24:53 -0700

    flake: Refactor overrides overlay

--- nix/overlays/overrides/default.nix
+++ nix/overlays/overrides/default.nix
@@ -1,54 +1,63 @@
 # Copyright (C) Archit Gupta <archit@accelbread.com>
 # SPDX-License-Identifier: AGPL-3.0-or-later
-final: prev: {
-  ccacheWrapper = prev.ccacheWrapper.override {
-    extraConfig = ''
-      export CCACHE_COMPRESS=1
-      export CCACHE_SLOPPINESS=random_seed
-      export CCACHE_DIR=/var/cache/ccache
-      export CCACHE_UMASK=007
-    '';
-  };
-  nut = prev.nut.overrideAttrs (old: {
-    postPatch = ">conf/Makefile.am";
-    configureFlags = old.configureFlags ++ [
-      "--with-drivers=usbhid-ups"
-      "--without-dev"
-      "--with-user=nut"
-      "--with-group=nut"
-      "--sysconfdir=/etc/nut"
-      "--with-statepath=/var/lib/nut"
-    ];
-  });
-  bees = prev.bees.overrideAttrs {
-    utillinux = final.runCommand final.util-linux.name
-      {
-        inherit (final.util-linux) meta pname version;
-        nativeBuildInputs = [ final.makeBinaryWrapper ];
-      } ''
-      cp -r ${final.util-linux} $out
+final: prev:
+let
+  inherit (builtins) any filter mapAttrs path;
+  inherit (final.lib) filesystem hasSuffix;
+in
+prev.lib.composeManyExtensions [
+  (final: prev: mapAttrs
+    (pkg: _: prev.${pkg}.overrideAttrs (old: {
+      patches = old.patches or [ ] ++
+        (map (p: path { path = p; })
+          (filter (p: any (e: hasSuffix e p) [ ".patch" ".mbx" ])
+            (filesystem.listFilesRecursive (./patches + "/${pkg}"))));
+    }))
+    (builtins.readDir ./patches))
+  (final: prev: {
+    ccacheWrapper = prev.ccacheWrapper.override {
+      extraConfig = ''
+        export CCACHE_COMPRESS=1
+        export CCACHE_SLOPPINESS=random_seed
+        export CCACHE_DIR=/var/cache/ccache
+        export CCACHE_UMASK=007
+      '';
+    };
+    nut = prev.nut.overrideAttrs (old: {
+      postPatch = ">conf/Makefile.am";
+      configureFlags = old.configureFlags ++ [
+        "--with-drivers=usbhid-ups"
+        "--without-dev"
+        "--with-user=nut"
+        "--with-group=nut"
+        "--sysconfdir=/etc/nut"
+        "--with-statepath=/var/lib/nut"
+      ];
+    });
+    bees = prev.bees.overrideAttrs {
+      utillinux = final.runCommand final.util-linux.name
+        {
+          inherit (final.util-linux) meta pname version;
+          nativeBuildInputs = [ final.makeBinaryWrapper ];
+        } ''
+        cp -r ${final.util-linux} $out
+        chmod -R u+w $out
+        wrapProgram $out/bin/mount --add-flags "-o noatime"
+      '';
+    };
+    rnote = final.runCommand prev.rnote.name { } ''
+      cp -Lr ${prev.rnote} $out
       chmod -R u+w $out
-      wrapProgram $out/bin/mount --add-flags "-o noatime"
+      rm -r $out/share/fonts
     '';
-  };
-  rnote = final.runCommand prev.rnote.name { } ''
-    cp -Lr ${prev.rnote} $out
-    chmod -R u+w $out
-    rm -r $out/share/fonts
-  '';
-  nautilus = prev.nautilus.overrideAttrs (old: {
-    preFixup = old.preFixup or "" + ''
-      gappsWrapperArgs+=(
-        --prefix XDG_DATA_DIRS : "${final.glycin-loaders}/share"
-      )
-    '';
-  });
-} // builtins.mapAttrs
-  (pkg: _: prev.${pkg}.overrideAttrs (old: {
-    patches = old.patches or [ ] ++
-      (map (p: builtins.path { path = p; })
-        (builtins.filter
-          (p: builtins.any (e: final.lib.hasSuffix e p) [ ".patch" ".mbx" ])
-          (final.lib.filesystem.listFilesRecursive (./patches + "/${pkg}"))));
-  }))
-  (builtins.readDir ./patches)
+    nautilus = prev.nautilus.overrideAttrs (old: {
+      preFixup = old.preFixup or "" + ''
+        gappsWrapperArgs+=(
+          --prefix XDG_DATA_DIRS : "${final.glycin-loaders}/share"
+        )
+      '';
+    });
+  })
+]
+  final
+  prev
